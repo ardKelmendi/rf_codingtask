5df41b3417775ca78346a6438569308618e85762
ardKelmendi <kelmeard@fit.cvut.cz>
2020-08-14 13:14:14 +0200

    editing test

--- test/test_post.py
+++ test/test_post.py
@@ -1,8 +1,36 @@
 # test_post.py
+import pytest
 from main import app
-from flask import json
+from flask import json, Flask
+import unittest
+from flask_testing import TestCase
+from flask_testing import LiveServerTestCase
 
 
+class MyTest(TestCase):
+
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        return app
+
+class MyTest(LiveServerTestCase):
+
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        # Default port is 5000
+        app.config['LIVESERVER_PORT'] = 8943
+        # Default timeout is 5 seconds
+        app.config['LIVESERVER_TIMEOUT'] = 10
+        return app
+
+    def test_server_is_up_and_running(self):
+        response = urllib2.urlopen(self.get_server_url())
+        self.assertEqual(response.code, 200)
+
+
+@pytest.fixture()
 def test_add():        
     response = app.test_client().post(
         '/api/categories',
@@ -14,4 +42,3 @@ def test_add():
 
     assert response.status_code == 200
     assert data['name'] == "ard"
-
