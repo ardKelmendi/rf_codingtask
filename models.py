0554ce23e75cd7f7ab089c591ebe39fa5451e24c
ardKelmendi <kelmeard@fit.cvut.cz>
2020-08-14 14:58:42 +0200

    Comments

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+#Class contains 3 classes that are used to store the data
+
 class Product:
     def __init__(self, id, name, unit, quantity, category, measurement_unit):
         self.id = id
@@ -7,6 +9,7 @@ class Product:
         self.category = category
         self.measurement_unit = measurement_unit
 
+    '''method to return a JSON format'''
     @property
     def serialize(self):
         return {
@@ -26,6 +29,7 @@ class Sales:
         self.units_sold = units_sold
         self.sold_date = sold_date
 
+    '''method to return a JSON format'''
     @property
     def serialize(self):
         return {
@@ -41,6 +45,7 @@ class Category:
         self.id = id
         self.name = name
 
+    '''method to return a JSON format'''
     @property
     def serialize(self):
         return {
